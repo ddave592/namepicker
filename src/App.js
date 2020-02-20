3ac3b699e2ef09b20ec677ce29e5d66e81f38c00
dgomes <david.g1591@googlemail.com>
2020-02-20 17:30:10 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import { TextField } from '@material-ui/core';
+import { TextField } from "@material-ui/core";
 
-import './styles/App.scss';
+import "./styles/App.scss";
+import { useEffect, useState } from "react";
+
+const [nameList, setNameList ] = useState([]);
 
 function App() {
+
   return (
-    <TextField id="filled-basic" label="Filled" variant="filled" />
-  );
+    <>
+  <TextField id="filled-basic" label="Filled" variant="filled" />
+  <div>
+    {nameList.map( (item,index) => {
+      return 
+    }
+    )}
+  </div>
+  </>
+  )
 }
 
 export default App;
